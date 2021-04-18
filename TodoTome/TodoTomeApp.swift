2ca7c8fdad7caee7a0cda208d72cded0447f01fd
phonggit3699 <phamphong3699@gmail.com>
2021-04-18 09:01:45 +0700

    Initial Commit

---
+++ TodoTome/TodoTomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTomeApp.swift
+//  TodoTome
+//
+//  Created by PHONG on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoTomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
